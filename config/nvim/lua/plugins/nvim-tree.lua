d4fa4ec6f9e51f65cc2e2840ecf7f04e7713d183
Mike Mannix <mike.mannix@contractors.mailchimp.com>
2024-03-19 13:31:59 -0400

    nvim things again

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -6,7 +6,28 @@ return {
     "nvim-tree/nvim-tree.lua",
     config = function()
         require("nvim-tree").setup({
+            view = {
+                width = 35,
+                relativenumber = true,
+            },
+            renderer = {
+                indent_markers = {
+                    enable = true,
+                },
+            },
+            filters = {
+                custom = { ".DS_Store" },
+            },
+            git = {
+                ignore = false,
+            },
         })
+
+        local keymap = vim.keymap
+        keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>")
+        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>")
+        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>")
+        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>")
     end,
 }
 
--- config/nvim/lua/remap.lua
+++ config/nvim/lua/remap.lua
@@ -4,3 +4,5 @@ vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
 vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("i", "jk", "<Esc>")
+
+vim.keymap.set("n", "<leader>q", "<cmd>qa<CR>")
