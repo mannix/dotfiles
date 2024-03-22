ad47cb59998d14b9f3868e58306ea46a3b1c8795
Mike Mannix <mike.mannix@contractors.mailchimp.com>
2024-03-22 16:47:42 -0400

    Tokyo night

---
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -0,0 +1,15 @@
+return {
+    "folke/tokyonight.nvim",
+    lazy = false,
+    priority = 1000,
+    opts = {},
+    config = function()
+        vim.cmd.colorscheme("tokyonight-moon")
+    end,
+}
+-- {
+--     "rebelot/kanagawa.nvim",
+--     config = function()
+--         vim.cmd.colorscheme("kanagawa-dragon")
+--     end,
+-- }
