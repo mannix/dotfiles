34e78aa18da55d4c9704eacfbec85a8190544b5f
Mike Mannix <mike.mannix@contractors.mailchimp.com>
2024-08-02 15:54:43 -0400

    changes

--- config/nvim/lua/plugins/colorscheme.lua
+++ config/nvim/lua/plugins/colorscheme.lua
@@ -1,13 +1,38 @@
+-- return {
+--     'metalelf0/jellybeans-nvim',
+--     dependencies = {
+--         {
+--             'rktjmp/lush.nvim'
+--         }
+--     },
+--     lazy = false,
+--     priority = 1000,
+--     config = function()
+--         vim.cmd.colorscheme("jellybeans-nvim")
+--     end,
+-- }
+
 return {
-    'metalelf0/jellybeans-nvim',
-    dependencies = {
-        {
-            'rktjmp/lush.nvim'
-        }
-    },
-    lazy = false,
-    priority = 1000,
+    'catppuccin/nvim',
     config = function()
-        vim.cmd.colorscheme("jellybeans-nvim")
+        vim.cmd.colorscheme("catppuccin-latte")
     end,
 }
+
+
+-- return {
+--     'projekt0n/github-nvim-theme',
+--     config = function()
+--         vim.cmd.colorscheme("github_light_default")
+--     end,
+-- }
+
+-- return {
+--     "folke/tokyonight.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     opts = {},
+--     config = function()
+--         vim.cmd.colorscheme("tokyonight-day")
+--     end,
+-- }
