96400c535ad4954f7e7cdedfff9c8ac71b4931f0
Mike Mannix <mike.mannix@gmail.com>
2025-09-23 11:25:53 -0400

    Nvim tree adaptive width

--- config/nvim/lua/plugins/nvim-tree.lua
+++ config/nvim/lua/plugins/nvim-tree.lua
@@ -1,9 +1,15 @@
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 return {
   "nvim-tree/nvim-tree.lua",
   version = "*",
   lazy = false,
   config = function()
     require("nvim-tree").setup({
+      view = {
+        adaptive_size = true,
+      },
       vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>')
     })
   end,
